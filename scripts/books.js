604204aa4a183368322af081233b000c65dad0a2
Jason Clifton <j.clifton1473@gmail.com>
2017-07-12 11:52:13 -0600

    User can now remove a book.

--- scripts/books.js
+++ scripts/books.js
@@ -3,43 +3,96 @@ const API_URL = 'http://localhost:3000/api/v1'
 
 function appready() {
   $('.modal').modal();
-  getBooks();
-  clickBooks();
   addBooks();
+  removeClick()
+  deleteBook()
+  if ($('body').hasClass('books-list')) {
+    getBooks();
+  } else if ($('body').hasClass('remove-book')) {
+    removePage();
+  }
+}
+
+function removeClick() {
+  $('main').on('click', '.remove-button', function(event) {
+    let id = $(this).attr('data-id')
+    console.log(id);
+    event.preventDefault();
+    console.log("clicked!");
+    window.location = `/delete.html?id=${id}`
+  })
+}
+
+function deleteBook() {
+  $('main').on('click', '.delete-button', function(event) {
+    console.log('clicked');
+    let id = $(this).attr('data-id')
+    event.preventDefault();
+    $.ajax({
+      url: `${API_URL}/books/${id}/delete`,
+      type: 'DELETE',
+      success: function(result) {
+        window.location = 'books.html'
+      }
+    })
+  })
+}
+
+function removePage() {
+  let url = window.location.href
+  let splitURL = url.split('=');
+  let id = splitURL[splitURL.length - 1];
+  $.get(`${API_URL}/books/${id}`)
+    .then(book => {
+      console.log(book);
+      removeBook(book);
+    })
 }
 
 function getBooks() {
   $.get(`${API_URL}/books`)
     .then(books => {
       displayBooks(books);
+      console.log(books);
     })
 }
 
 function displayBooks(books) {
   const source = $('#books-template').html();
   const template = Handlebars.compile(source);
-  const html = template({books});
+  const html = template({
+    books
+  });
   $('.books-display').append(html);
 }
 
-function clickBooks() {
-  $('.books-display').on('click', )
+function removeBook(book) {
+  const source = $('#remove-book-template').html();
+  console.log(source);
+  const template = Handlebars.compile(source);
+  const html = template(book);
+  $('.book-display').append(html);
 }
+
 function addBooks() {
-  $('.book-submit').on('submit' (event) => {
+  $('.book-submit').on('submit', (event) => {
     event.preventDefault();
     const newBookInfo = getAddedBookInfo();
+    $.post(`${API_URL}/books/new`, newBookInfo).then(results => {
+      window.location = books.html;
+    })
   })
 }
 
 function getAddedBookInfo() {
-  let title = $('.add-book-title').val();
+  let bookTitle = $('.add-book-title').val();
   let imageURL = $('.add-book-image-url').val();
-  let genre = $('.add-book-genre').val();
-
+  let bookGenre = $('.add-book-genre').val();
+  let bookDescription = $('.add-book-description').val();
   return {
-    name: userName,
-    email: userEmail,
-    password: userPassword
+    title: bookTitle,
+    genre: bookGenre,
+    description: bookDescription,
+    cover_url: imageURL
   }
 }
